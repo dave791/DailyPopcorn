b44a6c6072e6ee8373ca74a8964e1c149d079908
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2019-10-30 16:40:47 -0400

    Created User model, tests pending

--- api/models/user.js
+++ api/models/user.js
@@ -3,14 +3,15 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {}
-  
+
   User.init({
     username: {
       type: DataTypes.STRING,
       validate: {
         notEmpty: true,
         notNull: true,
-      }
+      },
+      unique: true
     },
     email: {
       type: DataTypes.STRING,
@@ -18,7 +19,8 @@ module.exports = (sequelize, DataTypes) => {
         notEmpty: true,
         notNull: true,
         isEmail: true,
-      }
+      },
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
@@ -39,6 +41,10 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user'
   });
+
+  User.associate = (models) => {
+    //TODO (ElvisRodriguez) add associations to Post and Comment models
+  };
 
   return User;
 };
