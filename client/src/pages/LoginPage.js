3aa7e6f0a5137761ba80388f6a59ff1bdebf09ba
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2019-11-08 18:09:01 -0500

    Fixed background image

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -4,29 +4,29 @@ import { Redirect } from 'react-router-dom';
 class LoginPage extends React.Component {
   render() {
     return (
-          <div className="form-inline login_image1">
-            <div className='col-12 log-in'><h1>Login</h1></div>
+        <div className="col-12 welcome_image">
+          <div className="form-inline">
             <div className="col center-block">
-
-              <div className="input-group">
+              <div className="col-sm-12 col-md-12 col-12">
                 <input
                   type="text"
                   placeholder="username"
                   className="form-control mr-3 rounded log-in"
                 />
               </div>
-            </div>
-          <div className="form-group mr-2">
-            <div className="col-sm-9 col-md-9 ">
-              <input
-                type="password"
-                placeholder="password"
-                className="form-control mr-6 rounded log-in"
-              />
+              <div className="col-sm-12 col-md-12 col-12">
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="form-control mr-3 rounded log-in"
+                />
+              </div>
+              <button className="btn btn-primary button center-button log-in">
+              Login
+              </button>
             </div>
           </div>
-            <button className="btn btn-primary button center-button log-in "> Login </button>
-          </div>
+        </div>
     );
   }
 }
