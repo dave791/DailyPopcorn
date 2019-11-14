4481c4fb6f5ca4cdc6cc38468f22509b3089e493
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2019-11-14 01:55:38 -0500

    working on heroku issues

--- api/app.js
+++ api/app.js
@@ -5,10 +5,9 @@ const morgan = require('morgan');
 const path = require('path');
 const db = require('./models');
 const passport = require('./middlewares/authentication');
-const dotenv = require('dotenv');
+console.log(process.env.NODE_ENV);
+const dotenv = require('dotenv').config({path: '../.env'});
 const app = express();
-
-dotenv.config({path: '../.env'});
 
 const PORT = process.env.PORT || 8080;
 
