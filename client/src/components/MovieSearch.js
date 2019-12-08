ff0e7708aedbc24101f0fdc29fc0a45256f28d36
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2019-12-08 18:32:27 -0500

    Finished migrating Movie search components to main application, search currently broken, pushing only to iterate better with team members

--- client/src/components/MovieSearch.js
+++ client/src/components/MovieSearch.js
@@ -1,21 +1,105 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useReducer, useEffect } from "react";
+import "../App.css";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
-class MovieSearch extends React.Component {
-  render() {
-    return (
-      <div className="col-12 col-md-12 col-lg-12">
-        <div className="input-group">
-         <input
-           type="text"
-           placeholder="Search for a Movie"
-           className="form-control mr-3 rounded"
-         />
-         <button className="btn btn-primary"> Search </button>
-        </div>
-      </div>
-    );
+
+const MOVIE_API_URL = "http://www.omdbapi.com/?s=man&apikey=5eca414";
+//http://www.omdbapi.com/?t=&apikey=5eca414
+
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+};
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: 'Cannot complete search request'
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    default:
+      return state;
   }
-}
+};
+
+
+
+const MovieSearch = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+
+        fetch(MOVIE_API_URL)
+            .then(response => response.json())
+            .then(jsonResponse => {
+
+            dispatch({
+                type: "SEARCH_MOVIES_SUCCESS",
+                payload: jsonResponse.Search
+        	});
+      	});
+  	}, []);
+
+    const search = searchValue => {
+    	dispatch({
+      	type: "SEARCH_MOVIES_REQUEST"
+    	});
+
+        fetch(`https://www.omdbapi.com/?t=${searchValue}&apikey=5eca414`)
+      	.then(response => response.json())
+      	.then(jsonResponse => {
+        	if (jsonResponse.Response === "True") {
+          	dispatch({
+                type: "SEARCH_MOVIES_SUCCESS",
+                payload: jsonResponse.Search
+          	});
+        	} else {
+          	dispatch({
+                type: "SEARCH_MOVIES_FAILURE",
+                error: jsonResponse.Error
+          	});
+          }
+      	});
+	  };
+
+    const { movies, errorMessage, loading } = state;
+
+    return (
+    <div className="App">
+      <Header text="The Daily Popcorn" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading... </span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default MovieSearch;
